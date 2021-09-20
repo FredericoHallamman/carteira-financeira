bc29cdeeb221b676bf4be4bd4a6dfb5dea1cb45b
FredericoHallamman <fred.hallamman@gmail.com>
2021-09-20 01:55:44 +0100

    Update carteira GITHUB.py

--- carteira GITHUB.py
+++ carteira GITHUB.py
@@ -3,3 +3,6 @@ nome = n.title()
 cores = {'vermelho':'\033[1;31m', 'limpa':'\033[m'}
 print('Olá {}{}{} Prazer te conhecer !!!'.format(cores['vermelho'], nome, cores['limpa']))
 print('Vou te auxiliar na sua gestão financeira  vamos começar !!!')
+
+
+# EM CONSTRUÇÃO ...
