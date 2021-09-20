a5d4399b8e49d624ab706f23144c700680b2e92b
FredericoHallamman <fred.hallamman@gmail.com>
2021-09-20 01:53:37 +0100

    Create carteira GITHUB.py

---
+++ carteira GITHUB.py
@@ -0,0 +1,5 @@
+n = str(input('Digite seu nome: ')).strip()
+nome = n.title()
+cores = {'vermelho':'\033[1;31m', 'limpa':'\033[m'}
+print('Olá {}{}{} Prazer te conhecer !!!'.format(cores['vermelho'], nome, cores['limpa']))
+print('Vou te auxiliar na sua gestão financeira  vamos começar !!!')
